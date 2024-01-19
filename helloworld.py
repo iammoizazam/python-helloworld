955a0aae0d594a7fc028b784c38f6dae767ffd33
moiz azam <moizazam.azam@gmail.com>
2024-01-19 10:37:08 +0000

    added test file

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
-"""Top-level script to invoke helloworld implementation."""
+#"""Top-level script to invoke helloworld implementation."""
 
-import sys
-import helloworld.main
+#import sys
+#import helloworld.main
 #
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
+#if __name__ == '__main__':
+#i    sys.exit(helloworld.main.main())
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
