e75e6b7593a559c0ba76001bea663344867bd45e
moiz azam <moizazam.azam@gmail.com>
2024-01-22 12:17:51 +0000

    test.py update 6

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from helloworld import add
+from helloworld import *
 
 def test_add():
-    assert add(2,3) == 6
+    assert add(2,3) == 5
 
 def test_subtract():
-    assert subtract(2, 3) == -1
+    assert subtract(2, 3) == -3
 
 def test_multiply():
     assert multiply(2, 3) == 6
